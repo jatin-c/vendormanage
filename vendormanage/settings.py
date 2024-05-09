7e5788cb1f1816deca9d77c2c09c3d00b3baea39
Jatin Chauhan <jatinc574@gmail.com>
2024-05-09 13:10:27 +0530

    setting done

--- vendormanage/settings.py
+++ vendormanage/settings.py
@@ -17,8 +17,7 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
